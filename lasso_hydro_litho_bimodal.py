e264ea97467b6819a0d94ef1ae638180278ae237
daviddralle <daviddralle@macbook-pro.lan>
2021-04-07 15:36:39 -0700

    updated flow

--- lasso_hydro_litho_bimodal.py
+++ lasso_hydro_litho_bimodal.py
@@ -14,15 +14,20 @@ import warnings
 import sklearn
 from sklearn.metrics import r2_score
 
-def a(t, alpha, v0):
+def a(t, x0):
+	alpha, v0 = x0[0], x0[1]
     return alpha*v0*(1+t*v0)**(-1-alpha)
 
 def fv(v,alpha,v0):
     return 1/(scipy.special.gamma(alpha)*v0)*np.exp(-v/v0)*(v/v0)**(alpha-1)
 
-def get_rechargefit(x0,Q, positive=True):
+def a_two_gammas(t, x0):
+	alpha1,v01,alpha2,v02,w1 = x0[0],x0[1],x0[2],x0[3],x0[4]
+	return w1*alpha1*v01*(1+t*v01)**(-1-alpha1) + (1-w1)*alpha2*v02*(1+t*v02)**(-1-alpha2)
+ 
+def get_rechargefit(x0,Q,iuh,positive=True):
     t = np.arange(len(Q))
-    A = a(t, x0[0], x0[1])
+    A = iuh(t, x0)
     # create toeplitz matrix
     # performs convolution of IUH on input vector
     num_cols = len(Q)
@@ -40,7 +45,7 @@ def get_rechargefit(x0,Q, positive=True):
         idx_train = np.random.choice(range(int(len(Q)/2),len(Q)-1), int(len(Q)*frac), replace=False)
         idx_test = list(set(np.arange(int(len(Q)/2),len(Q))).difference(set(idx_train)))
         traintest.append((idx_train, idx_test))
-    clf = linear_model.LassoCV(cv=traintest, positive=positive, tol=1e-2)
+    clf = linear_model.LassoCV(cv=traintest, positive=positive, tol=1e-4)
     try:
         clf.fit(H, Q)
     except:
@@ -48,8 +53,8 @@ def get_rechargefit(x0,Q, positive=True):
 
     return clf, np.copy(H), np.copy(Q), t
 
-def get_score(x0,Q, positive=True, logtransform=True):
-    clf, H, Q, t = get_rechargefit(x0,Q,positive)
+def get_score(x0,Q,iuh,positive=True, logtransform=True):
+    clf, H, Q, t = get_rechargefit(x0,Q,iuh,positive)
     if clf==-1:
         return 1e13
     rechargefit = clf.coef_
@@ -66,23 +71,31 @@ def get_score(x0,Q, positive=True, logtransform=True):
             return 1-r2_score(Q,Qfit)
 
 def getmin(args):
-    f,x0,Q = args
-    res = minimize(f, x0,method='Nelder-Mead', tol=1e-2, args = (Q))
+    f,x0,Q,iuh = args
+    res = minimize(f, x0,method='Nelder-Mead', tol=1e-3, args = (Q, iuh))
     return res
 
 def main():
     df = pd.read_csv('./flow.csv', index_col=0)
     sites = df.columns.values
+
+    iuh = a_two_gammas
+    params = [np.random.uniform(low=0.2,high=1.5),
+    			 np.random.uniform(low=0.2,high=1.5),
+    			 np.random.uniform(low=0.2,high=1.5), 
+    			 np.random.uniform(low=0.2,high=1.5),
+    			 np.random.uniform(low=0.0,high=1.0)
+    			 ]
+
     ress = []
     for col in df.columns:
-        x0 = [0.5, 0.5]
         Q = df[col].values
         Q = Q[np.isfinite(Q)]
         idx = np.min([len(Q), 2000])
         Q = Q[-idx:]
         num = mp.cpu_count()
         p = mp.Pool(num)
-        args = [(get_score,[np.random.uniform(low=0.2,high=1.5), np.random.uniform(low=0.2,high=1.5)],Q) for i in range(num)]
+        args = [(get_score,params,Q,iuh) for i in range(num)]
         res_candidates = p.map(getmin,args)
         idx = np.argmin([item.fun for item in res_candidates])
         res = res_candidates[idx]
